ca45f4afb7fbc24d8533d1bdd66444c8f7054a85
Matthew Stern <matt.st97@gmail.com>
2025-02-18 19:31:43 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='optopenhash',
-    version='0.1.0',
+    version='0.2.0',
     author='Matthew Stern',
     author_email='matthew.stern@ztsystems.com',
     description=('A Python package implementing improved open‐addressing hash tables '
